c9d8a2c7c4f9741630093db181630132483840ed
Ryan Du <ryan_9809@yahoo.com>
2019-01-22 10:23:59 -0800

    First test commit

---
+++ mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //comment to test commit
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
