1d72af533d192422ea33625316d6d2fdeee8514e
Ryan Du <ryan_9809@yahoo.com>
2019-01-22 16:56:15 -0800

    Made a draft main window

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QLabel>
+#include <QPushButton>
+#include <QGridLayout>
 
 int main(int argc, char *argv[])
 {
     // Comment to check Slack interface
     
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+
     //MainWindow w;
     //w.show();
 
-    QLabel *label = new QLabel("Hello Qt!");
-    label->show();
+    QWidget *window = new QWidget;
+    window -> setWindowTitle("Main Entry Menu:");
 
-    return a.exec();
+    window->show();
+
+    QLabel *title_prompt = new QLabel("Choose the feature:");
+    QPushButton* button_Euler = new QPushButton("Euler's method");
+    QPushButton* button_Bisec = new QPushButton("Bisection");
+    QPushButton* button_Inter = new QPushButton("Interpolation");
+
+    //QObject::connect(button_Euler, SIGNAL(clicked()), &app, SLOT(quit()));
+    QGridLayout *layout = new QGridLayout;
+    layout -> addWidget(title_prompt,0,0);
+    layout -> addWidget(button_Euler,1,0);
+    layout -> addWidget(button_Bisec,2,0);
+    layout -> addWidget(button_Inter,3,0);
+
+    window->setLayout(layout);
+
+    return app.exec();
 }
