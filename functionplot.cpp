ba22338a36fa7fd07c9cd1eee0c34afb33ba1758
Ryan Du <ryan_9809@yahoo.com>
2019-02-09 21:44:25 -0800

    Function parser working now

--- functionplot.cpp
+++ functionplot.cpp
@@ -1,12 +1,49 @@
+/**
+ * This file is part of PIC 10C Final Project - A Graphic Calculator.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ * We declare that this work is our own.
+ * We did this work honestly and can fully stand behind everything that we have written.
+ * We did not copy code from anyone, student or otherwise, expect files we downloaded and have the permission to use.
+ * We pledge that we have neither given nor received unauthorized assistance on making this project.
+
+ * @file functionplot.cpp
+ * @brief The implimentation file for the function plot window.
+ *
+ * This is the function plot window for the project.
+ *
+ * @author Ryan Du (Empyreal092)
+ * @author Firat Taxpulat (FT-1984)
+ * @author Anita Chen (achen1105)
+ *
+ * @bug No known bugs, but more features to be added
+ */
+
 #include "functionplot.h"
 #include "qcustomplot.h"
 #include "ui_functionplot.h"
 
 FunctionPlot::FunctionPlot(QWidget *parent) :
-    QWidget(parent), ui(new Ui::FunctionPlot){
+    QWidget(parent), ui(new Ui::FunctionPlot), function_str(){
 
+    function_str = "25x^5 - 35x^4 - 15x^3 + 40x^2 - 15x + 1"; 
+	// this the an example, that the function parser works, now it is possible to take input from user and plot the function
+    initial = -1.0; // initial value
+    final = 1.0; // final value
     ui->setupUi(this);
-    FunctionPlot::makeplot();
+    FunctionPlot::makeplot<double>(); // call the make plot function
 }
 
 FunctionPlot::~FunctionPlot()
@@ -14,22 +51,47 @@ FunctionPlot::~FunctionPlot()
     delete ui;
 }
 
-
+template <typename T>
 void FunctionPlot::makeplot(){
+
+	// exprtk commands
+    typedef exprtk::symbol_table<T> symbol_table_t; 
+    typedef exprtk::expression<T>     expression_t;
+    typedef exprtk::parser<T>             parser_t;
+
+    std::string expr_string = function_str; // the function string
+
+    T x = T(initial); // initialize x
+
+    symbol_table_t symbol_table;
+    symbol_table.add_variable("x",x); // add x as a variable
+
+	// exprtk commands to parse the function
+    expression_t expression;  
+    expression.register_symbol_table(symbol_table);
+	parser_t parser;
+    parser.compile(expr_string,expression);
+
+    const T delta = T(1/1000.0); // the step size
+
     //plot functions
-    QVector<double> x(101), y(101); // initialize with entries 0..100
-    for (int i=0; i<101; ++i)
+    QVector<T> var, value; // the variable and value vector
+
+    for (x = T(initial); x <= T(final); x += delta) // for all value points
     {
-      x[i] = i/50.0 - 1; // x goes from -1 to 1
-      y[i] = x[i]*x[i];  // let's plot a quadratic function
+       T result = expression.value(); // evaluate the result of the function string
+       var.push_back(x); // add the variable
+       value.push_back(result); // add the result 
     }
-    // create graph and assign data to it:
+    // create graph and assign data to it
     ui->customPlot->addGraph();
-    ui->customPlot->graph(0)->setData(x, y);
-    // give the axes some labels:
+    ui->customPlot->graph(0)->setData(var, value);
+    // give the axes some labels
     ui->customPlot->xAxis->setLabel("x");
     ui->customPlot->yAxis->setLabel("y");
-    // set axes ranges, so we see all data:
-    ui->customPlot->xAxis->setRange(-1, 1);
-    ui->customPlot->yAxis->setRange(0, 1);
+    // set axes ranges, so we see all data
+    T max = *std::max_element(value.begin(), value.end());
+    T min = *std::min_element(value.begin(), value.end());
+    ui->customPlot->xAxis->setRange(T(initial)-0.1*abs(T(initial)), T(final)+0.1*abs(T(final)));
+    ui->customPlot->yAxis->setRange(min-0.1*abs(min), max+0.1*abs(max));
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,36 @@
+/**
+ * This file is part of PIC 10C Final Project - A Graphic Calculator.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ * We declare that this work is our own.
+ * We did this work honestly and can fully stand behind everything that we have written.
+ * We did not copy code from anyone, student or otherwise, expect files we downloaded and have the permission to use.
+ * We pledge that we have neither given nor received unauthorized assistance on making this project.
+
+ * @file mainwindow.cpp
+ * @brief The implimentation file for the entry main window.
+ *
+ * This is the entry window for the project.
+ *
+ * @author Ryan Du (Empyreal092)
+ * @author Firat Taxpulat (FT-1984)
+ * @author Anita Chen (achen1105)
+ *
+ * @bug No known bugs, but more features to be added
+ */
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "functionplot.h"
@@ -6,27 +39,24 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
 
-    setWindowTitle("Main Entry Menu:");
+    setWindowTitle("Main Entry Menu:"); // set window title
 
-    title_prompt = new QLabel("Choose the features:");
-    button_Plot = new QPushButton("Plot Function");
-    button_Euler = new QPushButton("Euler's method");
-    button_Bisec = new QPushButton("Bisection");
+    title_prompt = new QLabel("Choose the features:"); // the prompt
+    button_Plot = new QPushButton("Plot Function"); // the plot function button
+    button_Euler = new QPushButton("Euler's method"); // the Euler's method button
+    button_Bisec = new QPushButton("Bisection"); // the Bisection window button
 
-    //QObject::connect(button_Euler, SIGNAL(clicked()), &app, SLOT(quit()));
-    centerlayout = new QGridLayout;
-    centerlayout -> addWidget(title_prompt,0,0,Qt::AlignCenter);
+    centerlayout = new QGridLayout; // layout
+    centerlayout -> addWidget(title_prompt,0,0,Qt::AlignCenter); // put objects in the layout
     centerlayout -> addWidget(button_Plot,1,0);
     centerlayout -> addWidget(button_Euler,2,0);
     centerlayout -> addWidget(button_Bisec,3,0);
 
-    centerpiece = new QWidget;
-
+    centerpiece = new QWidget; // set the central widget
     centerpiece->setLayout(centerlayout);
-
     setCentralWidget(centerpiece);
 
-    QObject::connect(button_Plot, SIGNAL(clicked()), this, SLOT(MakePlotWindow()));
+    QObject::connect(button_Plot, SIGNAL(clicked()), this, SLOT(MakePlotWindow())); // connect plot window bottom pressed to make the plot function window
 }
 
 MainWindow::~MainWindow()
@@ -35,6 +65,6 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::MakePlotWindow(){
-    FunctionPlot *p_window = new FunctionPlot(); // make the particle window
+    FunctionPlot *p_window = new FunctionPlot(); // make the function plot window
     p_window->show(); // show the window
 }
