ecd01435ffe9e1318dbbad894c77ad3867a173ca
Ryan Du <ryan_9809@yahoo.com>
2019-02-22 09:43:58 -0800

    Commented all the file except diffsolver.cpp

--- twoptrwindow.h
+++ twoptrwindow.h
@@ -1,3 +1,37 @@
+/**
+ * This file is part of PIC 10C Final Project - A Graphic Calculator.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ * We declare that this work is our own.
+ * We did this work honestly and can fully stand behind everything that we have written.
+ * We did not copy code from anyone, student or otherwise, expect files we downloaded and have the permission to use.
+ * We pledge that we have neither given nor received unauthorized assistance on making this project.
+
+ * @file twoptrwindow.h
+ * @brief The header file for the Two Point problem solver window.
+ *
+ * This is the Two Point problem solver window for the project.
+ *
+ * @author Ryan Du (Empyreal092)
+ * @author Firat Taxpulat (FT-1984)
+ * @author Anita Chen (achen1105)
+ *
+ * @bug No known bugs, but more features to be added
+ */
+
+
 #ifndef TWOPTRWINDOW_H
 #define TWOPTRWINDOW_H
 
@@ -8,11 +42,33 @@ namespace Ui {
 class TwoPtrWindow;
 }
 
+/**
+ * @class	TwoPtrWindow
+ *
+ * @brief	Two Point Value Problem window.
+ */
+
 class TwoPtrWindow : public FunctionPlot{
     Q_OBJECT
 
 public:
+
+    /**
+     * @fn	explicit TwoPtrWindow::TwoPtrWindow(FunctionPlot *parent = nullptr);
+     *
+     * @brief	Constructor
+     *
+     * @param [in,out]	parent	(Optional) If non-null, the parent.
+     */
+
     explicit TwoPtrWindow(FunctionPlot *parent = nullptr);
+
+    /**
+     * @fn	TwoPtrWindow::~TwoPtrWindow();
+     *
+     * @brief	Destructor
+     */
+
     ~TwoPtrWindow();
 };
 
