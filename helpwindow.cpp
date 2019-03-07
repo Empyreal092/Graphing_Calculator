ef60c56dce8a7bdfd43f74026b29776ffaadb6f8
Firat Taxpulat <fax215@gmail.com>
2019-03-06 18:34:18 -0800

    Set up scroll bar for help window and entered more detailed description for initial condition for odesolverbase.cpp

--- helpwindow.cpp
+++ helpwindow.cpp
@@ -1,13 +1,14 @@
 #include "helpwindow.h"
 #include <QGridLayout>
 #include <QTextStream>
+#include <QScrollArea>
+#include <QScrollBar>
 
 helpWindow::helpWindow()
 {
     //Reads the instructions text file
 
     instructionsTextFile = new QFile("C:/Users/fax21/Desktop/UCLA_Classes/PIC_10C/Final_Project/PIC10C_Final_Project-Graphic_Calc/Graphing Calculator Instructions Text File.txt");
-    //instructionsTextFile->open("Graphing Calculator Instructions Text File.txt")
     instructionsText = new QLabel;
     instructionsReadLine = new QString;
 
@@ -38,6 +39,14 @@ helpWindow::helpWindow()
     new_Help_Window->setWindowTitle("Help Window");
 
     new_Help_Window->setLayout(centerLayout);
+
+    //Adds a scrollbar to the help window
+
+    QScrollArea* scrollArea = new QScrollArea(this);
+    scrollArea->setWidget(instructionsText);
+    scrollArea->verticalScrollBar()->setSingleStep(new_Help_Window->width() / 24);
+
+    centerLayout->addWidget(scrollArea);
 
     //Sets the size of the help window
 
